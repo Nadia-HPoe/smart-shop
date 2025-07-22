5b12bda45f7d03e3d9a591aeaffb0d8390ff7b2e
Nadia-HPoe <heatherpoe.bn@gmail.com>
2025-07-22 12:15:40 +0200

    fix/conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import FAQ from '@/components/FAQ/FAQ';
-import styles from './page.module.scss';
-import Header from '@/components/Header/Header';
-import Story from '@/components/OurStory/Story';
-import Advantages from '@/components/Advantages/Advantages';
-<<<<<<< HEAD
-import Banner2 from '@/components/Banner2/Banner2';
-import Competitors from '@/components/Competitors/Competitors';
-=======
->>>>>>> 10a146366377ac48b44d618c9046f0d27f93d28e
+import FAQ from "@/components/FAQ/FAQ";
+import styles from "./page.module.scss";
+import Header from "@/components/Header/Header";
+import Story from "@/components/OurStory/Story";
+import Advantages from "@/components/Advantages/Advantages";
+import Banner2 from "@/components/Banner2/Banner2";
+import Competitors from "@/components/Competitors/Competitors";
 
 export default function Home() {
   return (
