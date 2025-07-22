c1d9437d969652748ff081c6336d58c92d066512
Nadia-HPoe <heatherpoe.bn@gmail.com>
2025-07-22 12:20:55 +0200

    fix/conflict

--- src/constants/GetAdvantagesItems.ts
+++ src/constants/GetAdvantagesItems.ts
@@ -1,43 +1,20 @@
-<<<<<<< HEAD
 export const advantageCards = [
   {
     id: 1,
-    img: '/images/advantages/icon1.png',
-    title: 'The price is two times \nlower than analogues',
-    text: 'We keep the price low (at cost level) since we have 11 more products in our ecosystem to increase business profitability, where we make money',
+    img: "/images/advantages/icon1.png",
+    title: "The price is two times \nlower than analogues",
+    text: "We keep the price low (at cost level) since we have 11 more products in our ecosystem to increase business profitability, where we make money",
   },
   {
     id: 2,
-    img: '/images/advantages/icon2.png',
-    title: 'Specialization \nexclusively in retail',
-    text: 'We have accumulated many years of experience, which is constantly updated and supplemented with the best new strategies, scenarios, visuals \nand other creatives.',
+    img: "/images/advantages/icon2.png",
+    title: "Specialization \nexclusively in retail",
+    text: "We have accumulated many years of experience, which is constantly updated and supplemented with the best new strategies, scenarios, visuals \nand other creatives.",
   },
   {
     id: 3,
-    img: '/images/advantages/icon3.png',
-    title: 'Team work',
-    text: 'Each client has 3-4 specialists plus a uniquely trained AI plus our weekly team meetings where we discuss the best solutions, results obtained and insights from the grocery retail market',
+    img: "/images/advantages/icon3.png",
+    title: "Team work",
+    text: "Each client has 3-4 specialists plus a uniquely trained AI plus our weekly team meetings where we discuss the best solutions, results obtained and insights from the grocery retail market",
   },
 ];
-=======
-export const advantageCards = [
-  {
-    id: 1,
-    img: '/images/advantages/icon1.png',
-    title: 'The price is two times \nlower than analogues',
-    text: 'We keep the price low (at cost level) since we have 11 more products in our ecosystem to increase business profitability, where we make money',
-  },
-  {
-    id: 2,
-    img: '/images/advantages/icon2.png',
-    title: 'Specialization \nexclusively in retail',
-    text: 'We have accumulated many years of experience, which is constantly updated and supplemented with the best new strategies, scenarios, visuals \nand other creatives.',
-  },
-  {
-    id: 3,
-    img: '/images/advantages/icon3.png',
-    title: 'Team work',
-    text: 'Each client has 3-4 specialists plus a uniquely trained AI plus our weekly team meetings where we discuss the best solutions, results obtained and insights from the grocery retail market',
-  },
-];
->>>>>>> 10a146366377ac48b44d618c9046f0d27f93d28e
